4019d384bb965fa3a6576b324bda2743c42030fa
Barath19 <bk190196@gmail.com>
2024-06-19 17:56:54 +0200

    updated corruption script

--- converter/convert_lidar.sh
+++ converter/convert_lidar.sh
@@ -1,7 +1,16 @@
-python3 lidar_converter.py \
---corruption snow \
---root_folder /workspace/nuscenes/nuscenes \
---dst_folder /workspace/corrupted_nuscenes/robust-nuscenes/snow/3/ \
---severity 3 \
---n_cpus 64 \
---sweep true
+#!/bin/bash
+
+multicorrupt_root="/home/bk/study/datasets/corrupted_dataset"
+nuscenes_data_dir="/home/bk/study/datasets/nuscenes_mini"
+
+severity_levels=("1" "2" "3")
+
+for severity in "${severity_levels[@]}"; do
+    python3 lidar_converter.py \
+    --corruption spatialmisalignment \
+    --root_folder $nuscenes_data_dir \
+    --dst_folder $multicorrupt_root/$severity/ \
+    --severity 3 \
+    --n_cpus 64 \
+    --sweep true
+done
